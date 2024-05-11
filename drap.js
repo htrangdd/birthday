ae0f299645abe965c3269a1c8b9c1d88b8361839
htrangdd <changdo2410@gmail.com>
2024-05-11 18:52:23 +0700

    Add files via upload

---
+++ drap.js
@@ -0,0 +1,9 @@
+
+const userNameElement = document.querySelector('.user-name');
+const passwordElement = document.querySelector('.pass-word');
+const userName = userNameElement.value;
+const pass = passwordElement.value;
+
+if (userName === 'lhnchi' && pass === '12345') {
+  `${.wrapper .btn a}`
+}
